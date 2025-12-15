8b59d2be7fbf59fe4ef5d0fb7cb43914f3e91074
Ramakrishnan <ramakrishnan@AGLS-MLT-1312.local>
2025-12-15 17:42:14 +0530

    Teams Integration

--- app.py
+++ app.py
@@ -2,7 +2,11 @@ from flask import Flask
 
 app = Flask(__name__)
 
-@app.route('/')
+@app.route("/")
 def home():
     return "Hello from Flask with Gunicorn on EC2!"
+
+@app.route("/health")
+def health():
+    return {"status": "ok"}, 200
 
